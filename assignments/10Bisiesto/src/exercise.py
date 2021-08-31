ea153aa54254b572031c3f6ac0efe8111d1f923e
AlbertoCS7 <89434313+AlbertoCS7@users.noreply.github.com>
2021-08-30 23:37:22 -0500

    Update exercise.py

--- assignments/10Bisiesto/src/exercise.py
+++ assignments/10Bisiesto/src/exercise.py
@@ -1,6 +1,14 @@
-
+def bisiesto(año):
+    if año%100==0 and año%400!=0:
+        return 'No es bisiesto :('
+    elif año%4==0 and a%100!=0:
+        return 'Si es bisiesto :)'
+    
 def main():
     #escribe tu código abajo de esta línea
+    año = int(input('Dime el año:'))
+    comprobacion = bisiesto(año)
+    print(comprobacion)
     pass
 
 if __name__=='__main__':
